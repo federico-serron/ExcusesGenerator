610afcf19ce680cc2a0947f758ff9b38d0baa857
Federico Serron Umpierrez <77068927+federico-serron@users.noreply.github.com>
2024-11-22 10:28:51 +0000

    Proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function generateExcuse(who, action, what, when) {
+    let sentence = "";
+    sentence =
+      who[Math.floor(Math.random() * who.length)] +
+      " " +
+      action[Math.floor(Math.random() * who.length)] +
+      " " +
+      what[Math.floor(Math.random() * who.length)] +
+      " " +
+      when[Math.floor(Math.random() * who.length)];
+
+    document.getElementById("excuse").innerHTML = sentence;
+  }
+
+  document.getElementById("btn").onclick = function() {
+    generateExcuse(who, action, what, when);
+  };
 };
